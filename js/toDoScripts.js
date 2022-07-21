b3fa70bd794f134854c26ebda96ea40ccc6a65f1
Lauro Aguirre Nascimento <laaauro@gmail.com>
2022-07-20 21:35:05 -0300

    Primeira etapa de codificação

---
+++ js/toDoScripts.js
@@ -0,0 +1,26 @@
+var GLOBAL_TASKS_NUMBER = 0
+
+function addTask() {
+  const taskText = document.getElementById('input-new-task').value
+  console.log(taskText)
+
+  if(taskText === '') return
+
+  const divNewItem = document.createElement('div')
+  divNewItem.className='task-item'
+  const newTask = document.createElement('input')
+  newTask.type = 'checkbox'
+  newTask.name = `task_${GLOBAL_TASKS_NUMBER}`
+  newTask.id = `task_${GLOBAL_TASKS_NUMBER}`
+  newTask.ariaLabel = taskText
+  const newTaskLabel = document.createElement('label')
+  newTaskLabel.htmlFor = `task_${GLOBAL_TASKS_NUMBER}`
+  newTaskLabel.textContent = taskText
+
+  divNewItem.append(newTask)
+  divNewItem.append(newTaskLabel)
+  const tasksList = document.getElementById('tasks-list')
+  tasksList.append(divNewItem)
+
+  GLOBAL_TASKS_NUMBER++
+}
